a07ab341eb88a71dca9acd31598020e78b4b38ce
Andrew Baik <and.baik@gmail.com>
2018-09-28 22:31:56 -0400

    scaffold files and setup project hierarchy

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: 'Cute Doggo',
+  description: 'im good boi',
+  // Customizing the default theme
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Projects', link: '/projects/' },
+      { text: 'Guide', link: '/guide/' }
+    ],
+    // Below two properties sets the Github link in the navbar
+    // Assumes GitHub. Can also be a full GitLab url.
+    repo: 'strychemi/vuepress-strychemi',
+    // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
+    repoLabel: 'GitHub',
+
+    // displays the UNIX timestamp(ms) of each file's last git commit at bottom of each page in an appropriate format
+    // NOTE: only works if this is  git repo and there is at least 1 commit
+    lastUpdated: 'Last Updated', // string | boolean
+
+    serviceWorker: {
+      updatePopup: true // Boolean | Object, default to undefined.
+      // If set to true, the default text config will be: 
+      // updatePopup: { 
+      //    message: "New content is available.", 
+      //    buttonText: "Refresh" 
+      // }
+    },
+  }
+};
