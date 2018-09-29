ab65de91cf5b93455a1f4436a1ac245cc9c3aeba
Andrew Baik <and.baik@gmail.com>
2018-09-29 01:47:10 -0400

    Progress on landing page

---
+++ docs/.vuepress/components/Landing.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <div class="hero">
+
+      <h1>{{ data.heroText || $title || 'Hello' }}</h1>
+
+      <p class="description">
+        {{ data.tagline || $description || 'Welcome to your VuePress site' }}
+      </p>
+
+      
+      <div>{{ this.$page }}</div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    data() {
+      return this.$page.frontmatter;
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,5 @@
-# Home Page
+---
+layout: Landing
+greeting: hello
+attribute: fire
+---
